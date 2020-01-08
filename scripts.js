e20df462886376046b469b1526d6bb69cf92f9a8
Shawn Donoho <shawn.donoho@slalom.com>
2020-01-08 17:21:00 -0600

    boxes!

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
+// set initial base hue to PANTONE™ 2020 Color of the Year (Classic Blue)
 const initialBaseHue = 204;
+
+// fetch browser window base width
 const initialWidth = window.innerWidth;
+
+// inject base hue as a custom property on the <HTML> element
 let style = document.documentElement.style;
 style.setProperty("--baseHue", initialBaseHue);
+
+// listen for window resize
 window.addEventListener("resize", updateStyle);
 
 function updateStyle() {
