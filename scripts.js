7ef5b757874e6a77fb71f8c8e918265967488a3b
Shawn Donoho <shawn.donoho@slalom.com>
2020-01-08 19:51:16 -0600

    template literals ftw

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 // set initial base hue to PANTONE™ 2020 Color of the Year (Classic Blue)
 const initialBaseHue = 204;
-const initialBaseLum = "100%";
-const initialBaseSat = "25%";
+const initialBaseLum = 100;
+const initialBaseSat = 25;
 
 // fetch browser window base dimensions
 const initialWidth = window.innerWidth;
@@ -10,8 +10,8 @@ const initialHeight = window.innerHeight;
 // inject base hue as a custom property on the <HTML> element
 let style = document.documentElement.style;
 style.setProperty("--baseHue", initialBaseHue);
-style.setProperty("--baseLum", initialBaseLum);
-style.setProperty("--baseSat", initialBaseSat);
+style.setProperty("--baseLum", `${initialBaseLum}%`);
+style.setProperty("--baseSat", `${initialBaseSat}%`);
 
 // listen for window resize
 window.addEventListener("resize", updateStyle);
