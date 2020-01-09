5e1fda97966288d76e70d4d39d7bf6409493d734
Shawn Donoho <shawn.donoho@slalom.com>
2020-01-08 21:17:01 -0600

    new lum/sat math

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 // set initial base hue to PANTONE™ 2020 Color of the Year (Classic Blue)
 const initialBaseHue = 204;
-const initialBaseLum = 100;
-const initialBaseSat = 25;
+const initialBaseLum = 400;
+const initialBaseSat = 100;
+const denominator = 4;
 
 // fetch browser window base dimensions
 const initialWidth = window.innerWidth;
@@ -10,15 +11,19 @@ const initialHeight = window.innerHeight;
 // inject base hue as a custom property on the <HTML> element
 let style = document.documentElement.style;
 style.setProperty("--baseHue", initialBaseHue);
-style.setProperty("--baseLum", `${initialBaseLum}%`);
-style.setProperty("--baseSat", `${initialBaseSat}%`);
+style.setProperty("--baseLum", `${Math.round(initialBaseLum / denominator)}%`);
+style.setProperty("--baseSat", `${Math.round(initialBaseSat / denominator)}%`);
 
 // listen for window resize
 window.addEventListener("resize", updateStyle);
 
 function updateStyle() {
   newHue = initialBaseHue - (initialWidth - window.innerWidth);
+  newLum = initialBaseLum + (initialHeight - window.innerHeight);
+  newSat = initialBaseSat + (initialHeight - window.innerHeight);
   style.setProperty("--baseHue", newHue);
+  style.setProperty("--baseLum", `${Math.round(newLum / denominator)}%`);
+  style.setProperty("--baseSat", `${Math.round(newSat / denominator)}%`);
 }
 
 // let size = `Width: ${w}, Height: ${h}`;
